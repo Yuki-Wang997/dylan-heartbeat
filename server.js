acbcc20b4bc98b4918fbe82af76fda2587a92492
Yuki-Wang997 <1179187969@qq.com>
2026-07-24 01:32:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
+const WebSocket = require("ws");
 const { createClient } = require("@supabase/supabase-js");
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_KEY,
+  { realtime: { transport: WebSocket }}
 );
 
 console.log("ALLOW_PUBLIC_API runtime:", process.env.ALLOW_PUBLIC_API);
