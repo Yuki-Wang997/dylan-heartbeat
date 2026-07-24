35acb5523870cd29341a4c64dd27ad1d85b2afad
Yuki-Wang997 <1179187969@qq.com>
2026-07-25 04:19:50 +0800

    Update wake_up.js

--- wake_up.js
+++ wake_up.js
@@ -684,8 +684,10 @@ ${messagesText}`
         console.log(`\n${pushResult.providerLabel} 推送失败，本次不发送推送\n`);
         eventContent = `（${getLocalTimeString()} 自动唤醒：本次未发送推送｜原因：${pushResult.providerLabel} 推送失败：${pushResult.reason}）`;
       } else {
-        eventContent = `（${getLocalTimeString()} 刚刚给用户发了${pushResult.providerLabel}推送：${safeTitle}｜${safeBody}）`;
-      }
+  saveWakeHistory(safeBody);
+
+  eventContent = `（${getLocalTimeString()} 刚刚给用户发了${pushResult.providerLabel}推送：${safeTitle}｜${safeBody}）`;
+}
     }
   }
 
